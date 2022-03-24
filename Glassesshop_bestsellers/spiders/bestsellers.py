c791a0f95335defe58f0f6b4900274a3d0530f1f
Juma Melvine <juma.study@gmail.com>
2022-03-25 00:19:34 +0300

    Added multi-page handling

--- Glassesshop_bestsellers/spiders/bestsellers.py
+++ Glassesshop_bestsellers/spiders/bestsellers.py
@@ -4,7 +4,7 @@ import scrapy
 class BestsellersSpider(scrapy.Spider):
     name = 'bestsellers'
     allowed_domains = ['www.glassesshop.com']
-    start_urls = ['https://https://www.glassesshop.com/bestsellers']
+    start_urls = ['https://www.glassesshop.com/bestsellers']
 
     def parse(self, response):
         products = response.xpath("//div[@id='product-lists']/div[@class='col-12 pb-5 mb-lg-3 col-lg-4 product-list-row text-center product-list-item']")
@@ -14,14 +14,17 @@ class BestsellersSpider(scrapy.Spider):
             yield{
                 "name" : product.xpath("normalize-space(.//div[@class='p-title']/a[1]/text())").get(),
                 "price" : product.xpath(".//div[@class='p-price'//span/text()").get(),
-                "image_link" : response.xpath(".//img[@class='lazy d-block w-100 product-img-default']/@src").get(),
+                "image_link" : product.xpath(".//img[@class='lazy d-block w-100 product-img-default']/@src").get(),
                 "product_url" : product.xpath(".//div[@class='p-title']/a[1]/@href").get()
 
 
             }
 
 
-            # next_page = response.xpath("")
+        next_page = response.xpath("//ul[@class='pagination']/li[position() = last()]/a/@href").get()
+        if next_page:
+            yield scrapy.Request(url = next_page, callback = self.parse)
+
 
             # product_name = product.xpath("normalize-space(.//div[@class='p-title']/a[1]/text())").get()
             # product_price = product.xpath(".//div[@class='p-price'//span/text()").get()
